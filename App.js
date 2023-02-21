649e76c870f6c29ce6265f2d13b7fb42aac50aeb
Tomos Heighway <tomosheighway@gmail.com>
2023-02-21 16:46:11 +0000

    Basic login page with validation on inputs

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, TextInput, View, Button } from 'react-native';
+import * as EmailValidator from 'email-validator'; // Importing the email-validator library
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      errorMessage: ""        // error state 4 outputting error message on login 
+    }
+  }
+
+  handleEmailInput = (email) => {
+    this.setState({email: email})
+  }
+
+  handlePasswordInput = (pass) => {
+    this.setState({password: pass})
+  }
+
+  handleLogin = () => {
+    // Validation login logic 
+    if (!this.state.email || !this.state.password) {
+      this.setState({errorMessage: "Please fill in all of the fields"});
+      return;
+    }
+    if (!EmailValidator.validate(this.state.email)) {
+      this.setState({errorMessage: "Please enter a valid email address"});
+      return;
+    }
+    if (!this.isStrongPassword(this.state.password)) {
+      this.setState({errorMessage: "Please enter a strong password. \n(8 or more characters including at least one uppercase letter, one number, and one special character: !@#$%^&*()_+-=[]{};:'\"\\|,.<>/?)"});
+      return;
+    }
+
+    //output msg 
+    this.setState({errorMessage: "Login successful!"});     //success message for testing. 
+  }
+
+  // Password strength check 
+  isStrongPassword = (password) => {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{8,}$/; //(including: one uppercase, one number and one special
+    return passwordRegex.test(password);
+  }
+  
+
+  render() {
+    return (
+      <View>
+        <TextInput placeholder='email...' onChangeText={this.handleEmailInput} value={this.state.email} />
+        <TextInput placeholder='password...' onChangeText={this.handlePasswordInput} value={this.state.password} secureTextEntry={true} />
+        <Button title="Login" onPress={this.handleLogin} />
+
+        {this.state.errorMessage ? <Text>{this.state.errorMessage}</Text> : null}
+      </View>
+    );
+  }
 }
-//comment test 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default App;
