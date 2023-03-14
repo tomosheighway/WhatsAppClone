15c7fe506ade1682840767f8e67df15c84d76c71
tomos <tomosheighway@gmail.com>
2023-03-14 10:51:00 +0000

    logout button on will remove details from async

--- components/home.js
+++ components/home.js
@@ -9,7 +9,7 @@ class Home extends Component {
     // home page for main app navigation 
     // tab nav to seperate pages 
     // need add logout button on one the pages as currecntly to logout you have to deleate values from async. 
-    
+
       componentDidMount(){
         this.unsubscribe = this.props.navigation.addListener('focus', () => {
             this.checkLoggedIn();
@@ -25,12 +25,50 @@ class Home extends Component {
         }
       }
 
+      async logout(){
+        console.log("logout")
+        return fetch("http://localhost:3333/api/1.0.0/logout",{
+            method: "POST",
+            headers: {
+                "X-Authorization": await AsyncStorage.getItem("session_token")
+            }
+        })
+        .then(async (response) => {
+            if(response.status ===200){
+                await AsyncStorage.removeItem("session_token")
+                await AsyncStorage.removeItem("user_id")
+                //this.props.navigation.navigate('Login')
+                //this.checkLoggedIn
+            } else if (response.status === 401){
+                console.log("Unauthroised")
+                await AsyncStorage.removeItem("session_token")
+                await AsyncStorage.removeItem("user_id")
+                //this.props.navigation.navigate('Login')
+                //this.checkLoggedIn
+            } else {
+                throw "something went wrong "
+            }
+        })
+        .catch((error) =>{
+            console.log("catch error ", error)
+        })
+      }
+
+
+
+
     //test render just to return output
     render() {
         return (
           <View>
             <TextInput placeholder='Test' />
             
+            <TouchableOpacity
+		        style={styles.buttonContainer}
+                onPress={this.logout}>
+			    <Text style={styles.buttonText}>Logout</Text>
+		    </TouchableOpacity>
+
           </View>
         );
     }
@@ -39,4 +77,51 @@ class Home extends Component {
 
 }
 
+
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#ebebeb'
+    },
+  
+    textInputContainer: {
+      marginBottom: 20,
+      width: '80%',
+      borderBottomColor: '#222',
+      borderBottomWidth: 1,
+      alignItems: 'center',
+      flexDirection: 'row'
+      },
+      textInput: {
+      fontSize: 18,
+      marginLeft: 5,
+      flex: 1,
+      padding: 10,
+      color: '#222'
+      },
+      textInputIcon: {
+      marginLeft: 5,
+      marginRight: 5
+      },
+  
+    text: {
+      color: '#101010',
+      fontSize: 24,
+      fontWeight: 'bold'
+    },
+    buttonContainer: {
+      backgroundColor: '#222',
+      borderRadius: 5,
+      padding: 10,
+      margin: 20
+    },
+    buttonText: {
+      fontSize: 20,
+      color: '#fff'
+    }
+  });
 export default Home
