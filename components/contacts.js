654575ccc6e46284982126e4f5f54eb3f8710899
tomos <tomosheighway@gmail.com>
2023-04-21 22:55:51 +0100

    restructure contacts to one page

--- components/contacts.js
+++ components/contacts.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as EmailValidator from 'email-validator';
 import {
-  View, Text, Button, TouchableOpacity,
+  View, Text, Button, TouchableOpacity, TextInput,
 } from 'react-native';
 
 class Contacts extends Component {
@@ -14,6 +15,8 @@ class Contacts extends Component {
     this.state = {
       contacts: [],
       blockedContacts: [],
+      email: '',
+      errorMessage: '',
     };
   }
 
@@ -27,6 +30,63 @@ class Contacts extends Component {
       this.setState({ blockedContacts });
     }
     console.log(contacts);
+  }
+
+  handleAddContact = async () => {
+    const { email } = this.state;
+    if (!EmailValidator.validate(email)) {
+      this.setState({ errorMessage: 'Please enter a valid email address' });
+      return;
+    }
+    console.log(`Email entered: ${email}`);
+    const data = await this.getUsersId(email);
+    console.log(`Data returned: ${JSON.stringify(data)}`);
+    if (data && data.length > 0) {
+      const userId = data[0].user_id;
+      console.log(`User ID: ${userId}`);
+      await this.addUserAsContact(userId);
+    } else {
+      this.setState({ errorMessage: 'That email address couldnt be linked with a user of this app' });
+      console.log('User not found');
+    }
+  };
+
+  async getUsersId(email) {
+    const sessionToken = await AsyncStorage.getItem('sessionToken');
+
+    return fetch(`http://localhost:3333/api/1.0.0/search?q=${email}&search_in=all`, {
+      method: 'GET',
+      headers: {
+        'X-Authorization': sessionToken,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(async (response) => {
+        if (response.status === 200) {
+          const data = await response.json();
+          return data;
+        }
+        if (response.status === 401) {
+          const { navigation } = this.props;
+          console.log('Unauthorized error');
+          navigation.navigate('Login');
+          return null;
+        }
+        if (response.status === 404) {
+          this.setState({ errorMessage: 'Not found :(' });
+          throw new Error('Not found ');
+        } else if (response.status === 500) {
+          this.setState({ errorMessage: 'Server Error :(' });
+          throw new Error('Server Error');
+        } else {
+          throw new Error('Something went wrong');
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        return null;
+      });
   }
 
   async getContacts() {
@@ -200,18 +260,66 @@ class Contacts extends Component {
       });
   }
 
+  async addUserAsContact(userId) {
+    const sessionToken = await AsyncStorage.getItem('sessionToken');
+
+    return fetch(`http://localhost:3333/api/1.0.0/user/${userId}/contact`, {
+      method: 'Post',
+      headers: {
+        'X-Authorization': sessionToken,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(async (response) => {
+        if (response.status === 200) {
+          console.log('Contact Added successfully');
+          const contacts = await this.getContacts();
+          const blockedContacts = await this.getBlockedContacts();
+          if (contacts && blockedContacts) {
+            this.setState({ contacts, blockedContacts });
+          }
+        } else if (response.status === 401) {
+          const { navigation } = this.props;
+          console.log('Unauthorized error');
+          navigation.navigate('Login');
+        } else if (response.status === 500) {
+          throw new Error('Server Error');
+        } else if (response.status === 400) {
+          this.setState({ errorMessage: 'You cant add yourself as a contact!' });
+          throw new Error('You cant add yourself');
+        } else {
+          throw new Error('Something went wrong');
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   render() {
-    const { contacts, blockedContacts } = this.state;
+    const { contacts, blockedContacts, errorMessage } = this.state;
     const { navigation } = this.props;
     return (
       <View>
+        {errorMessage ? <Text>{errorMessage}</Text> : null}
+        <Text>Enter the email of a contact you wish to add</Text>
+        <TextInput
+          placeholder="Email...."
+          onChangeText={(text) => this.setState({ email: text })}
+        />
 
-        <TouchableOpacity
+        <TouchableOpacity onPress={this.handleAddContact}>
+          <Text>Add</Text>
+        </TouchableOpacity>
+
+        {/* <TouchableOpacity
           onPress={() => navigation.navigate('AddContact')}
         >
           <Text>Add a new contact</Text>
-        </TouchableOpacity>
-        <Text> List of Users contacts</Text>
+        </TouchableOpacity> */}
+
+        <Text> ---------------------List of Users contacts--------------------</Text>
         {contacts.map((contact) => (
           <View key={contact.user_id}>
             <Text>
@@ -241,7 +349,7 @@ class Contacts extends Component {
 
         <Text>
           {'\n'}
-          List of Blocked contacts
+          ---------------------------List of Blocked contacts---------------------------
         </Text>
         {blockedContacts.map((blockedContact) => (
           <View key={blockedContact.user_id}>
