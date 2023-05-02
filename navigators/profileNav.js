d8de61b26d88f93b7756cbb2be988e16e79d9e15
tomos <tomosheighway@gmail.com>
2023-05-02 09:47:34 +0100

    navigate directly to camera

--- navigators/profileNav.js
+++ navigators/profileNav.js
@@ -4,7 +4,7 @@ import * as React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Profile from '../components/profile';
 import UpdateProfile from '../components/updateProfile';
-import CameraScreen from '../components/cameraScreen';
+import CameraScreen from '../components/camera-senttoserver';
 
 const Stack = createNativeStackNavigator();
 
